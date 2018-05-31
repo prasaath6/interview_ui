6a4a1a6fd289dbb91ed45dcb3dc26dbff1bccba4
Prasaath <prasaath@localhost.localdomain>
2018-05-31 07:39:51 +0530

    page 1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { AppRouting } from './app.routing';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRouting
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+    },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ],
+})
+export class AppRouting { }
